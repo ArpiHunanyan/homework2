295e408c78785fc842859ae69a92c8b2bdc2d82c
Apple <apple@Apples-MacBook.local>
2018-09-11 13:52:13 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,21 @@
+alert("hallo our new users" );
+  const ageA = 25;
+  const nameA = 'Ann';
+  const ageB = 29;
+  const nameB = 'Sus';
+  const age = 16
+   consol.log("mambers username");
+   consol.log(nameA+ageA);
+   consol.log(nameB+ageB);
+   consol.log("the avrage age of mambers");
+   consol.log((ageA+ageB)/2);
+    if (  ageA >= age ) { 
+      consol.log( nameA + "welcome to our group" ) 
+    } else { 
+      consol.log( nameA + "you are not allowded in the  group" );
+    }
+    if ( ageB >= age ) {
+      consel.log(nameB)"welcome to our group"
+    } else {
+      consel.log( nameB  + "you are not allowded in the group");
+    }
